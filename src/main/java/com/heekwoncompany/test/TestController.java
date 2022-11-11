f20b0178fb83b13cd383c3257ab29effcdba42da
gmlrnjsdla <dlagmlrnjs@gmail.com>
2022-11-11 17:33:12 +0900

    mybatis 설정 추가

--- src/main/java/com/heekwoncompany/test/TestController.java
+++ src/main/java/com/heekwoncompany/test/TestController.java
@@ -1,11 +1,18 @@
 package com.heekwoncompany.test;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.heekwoncompany.test.dao.IDao;
+
 @Controller
 public class TestController {
+	
+	@Autowired
+	private SqlSession sqlSession;
 	
 	@RequestMapping(value="test")
 	public String test(Model model) {
@@ -19,5 +26,20 @@ public class TestController {
 		
 		
 		return "test";
+	}
+	
+	@RequestMapping(value="insert")
+	public String insert() {
+		
+		IDao dao = sqlSession.getMapper(IDao.class);
+		
+		String mid = "tiger123";
+		String mpw = "12345";
+		String mname = "홍길순";
+		String memail = "tiger@gmail.com";
+		
+		dao.joinMemberDao(mid, mpw, mname, memail);
+		
+		return "insert";
 	}
 }
---
+++ src/main/java/com/heekwoncompany/test/dao/IDao.java
@@ -0,0 +1,7 @@
+package com.heekwoncompany.test.dao;
+
+public interface IDao {
+	
+	public void joinMemberDao(String mid, String mpw, String mname, String memail); //회원가입
+
+}
